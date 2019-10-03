f51a4ce81298d3717cee311913a3fe1910d4896f
nntan <tan.nguyen@dynagility.com>
2019-10-03 13:49:47 +0700

    'v1.03102019'

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,20 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTests {
+    private WebDriver driver;
+    public void setUp(){
+        System.setProperties("webdriver.chrome.driver", "resource/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+    public static void main(String[] args) {
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+}
